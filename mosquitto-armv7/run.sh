f82f6465138286c8c714e82742874e0764560de2
Abu uwais <uwaiscode@gmail.com>
2026-04-29 05:39:51 +0000

    Fix: Valid repository and PID 1 error

--- mosquitto-armv7/run.sh
+++ mosquitto-armv7/run.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/with-contenv bashio
 
-# Mengambil konfigurasi dari UI Home Assistant
+# Ambil Config dari UI HA
 USERNAME=$(bashio::config 'username')
 PASSWORD=$(bashio::config 'password')
 
-# Buat folder dan file password
-mkdir -p /etc/mosquitto
+# Buat file password baru setiap kali restart agar sinkron dengan UI
+bashio::log.info "Mempersiapkan autentikasi Mosquitto..."
 touch /etc/mosquitto/passwd
 mosquitto_passwd -b /etc/mosquitto/passwd "$USERNAME" "$PASSWORD"
 
-# Set izin akses agar Mosquitto tidak error (chmod 600)
+# Perbaiki permission agar Mosquitto tidak menolak file
 chown mosquitto:mosquitto /etc/mosquitto/passwd
 chmod 600 /etc/mosquitto/passwd
 
 bashio::log.info "Memulai Mosquitto Broker untuk user: $USERNAME"
 
-# Jalankan mosquitto
+# Jalankan mosquitto menggunakan 'exec' agar s6-overlay tetap menjadi PID 1
 exec mosquitto -c /etc/mosquitto/mosquitto.conf
